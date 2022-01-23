a86af7277c4cb9769bf5a30f831fa24c8448dd22
Thelight0804 <thelight0804@gmail.com>
2022-01-23 10:03:34 +0900

    Struing Funtion, list

---
+++ Files/220121_1_String Funtion.py
@@ -0,0 +1,6 @@
+console = "Nintendo Switch"
+print("console.count(i) = %d"%console.count("i")) #count : 문자 세기
+print("console.find(i) = %d"%console.find("i")) #find : 문자 찾기
+print("console.upper = %s"%console.upper()) #upper : 대문자로 바꾸기
+print("console.lower = %s"%console.lower()) #lower : 소문자로 바꾸기
+print("console.replace(\"Switch\", \"Wii\") = %s"%console.replace("Switch", "Wii")) #replace : 바꾸기
